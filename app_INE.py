bc55720037890461df9440151223da2e9b583d16
Jose-Fernandes <josesfernandes2013@gmail.com>
2025-12-15 14:21:07 -0100

    Update app_INE.py

--- app_INE.py
+++ app_INE.py
@@ -44,7 +44,7 @@ if menu == "Widgets":
   st.success(texto)
   
 if menu == "Graficos Estátiscos":
-  col1, col2, col3 = st.columns([0.45,0.1, 0.45])
+  col1, col2 = st.columns([1.5, 1.5])
   with col1:
     st.subheader("Coluna 1")
     dados_hist = [3,9,5,12,6,7,5,10,6,10]
