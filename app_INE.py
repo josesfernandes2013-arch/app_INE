8df888081e4d5a0c8e5ba3096faec2111a295149
Jose-Fernandes <josesfernandes2013@gmail.com>
2025-12-15 10:57:56 -0100

    Update app_INE.py

--- app_INE.py
+++ app_INE.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.header("Introduzindo os Elementos de Streamlit")
-menu =option_menu(menu_title="Menu",
+menu = option_menu(menu_title="Menu",
                   options=["Início", "Graficos Estátiscos", "Graficos Dinâmicos", "Widgets", "Formulário"],
                   icons=["house","bar-chart","bar-chart-line","toggles","bar-chart"],
                   menu_icon="cast",
