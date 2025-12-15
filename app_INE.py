6886d1f93b4fe7b5fa705c446deaaf8b108ab416
Jose-Fernandes <josesfernandes2013@gmail.com>
2025-12-15 11:18:07 -0100

    Update app_INE.py

--- app_INE.py
+++ app_INE.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 st.header("Introduzindo os Elementos de Streamlit")
 menu = option_menu(menu_title="Menu",
                   options=["Início", "Graficos Estátiscos", "Graficos Dinâmicos", "Widgets", "Formulário"],
-                  icons=["house-fill","bar-chart","bar-chart-line","toggles","ui-checks"],
+                  icons=["house-fill","bar-chart-fill","bar-chart-line-fill","toggles","ui-checks"],
                   menu_icon="cast",
                   default_index=0,
                   orientation="horizontal"
