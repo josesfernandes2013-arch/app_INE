30d67a5561e9a94aeba84d80b564745daf84c83d
Jose-Fernandes <josesfernandes2013@gmail.com>
2025-12-15 11:54:43 -0100

    Update app_INE.py

--- app_INE.py
+++ app_INE.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from streamlit_option_menu import option_menu 
 st.header("Introduzindo os Elementos de Streamlit")
 menu = option_menu(menu_title="Menu",
