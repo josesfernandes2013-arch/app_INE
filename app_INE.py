6249da3326170b7950b0ec22ac057be1a46d5786
Jose-Fernandes <josesfernandes2013@gmail.com>
2025-12-15 10:56:48 -0100

    Update app_INE.py

--- app_INE.py
+++ app_INE.py
@@ -3,7 +3,7 @@ st.header("Introduzindo os Elementos de Streamlit")
 menu =option_menu(menu_title="Menu",
                   options=["Início", "Graficos Estátiscos", "Graficos Dinâmicos", "Widgets", "Formulário"],
                   icons=["house","bar-chart","bar-chart-line","toggles","bar-chart"],
-                  menu_icon="cast"
-                  default_index=0
+                  menu_icon="cast",
+                  default_index=0,
                   orientation="horizontal"
   )
