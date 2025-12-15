102b4af046c92c28c3891b835f49aeaf2496565b
Jose-Fernandes <josesfernandes2013@gmail.com>
2025-12-15 10:59:16 -0100

    Update app_INE.py

--- app_INE.py
+++ app_INE.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu 
 st.header("Introduzindo os Elementos de Streamlit")
 menu = option_menu(menu_title="Menu",
                   options=["Início", "Graficos Estátiscos", "Graficos Dinâmicos", "Widgets", "Formulário"],
